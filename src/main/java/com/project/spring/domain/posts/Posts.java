cdfaba5038cf3147c9b2c78b2a6bc1a6be3e8cc7
Sangin-C <sangin3435@naver.com>
2021-05-12 00:39:04 +0900

    JPA Auditing 추가

--- src/main/java/com/project/spring/domain/posts/Posts.java
+++ src/main/java/com/project/spring/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.project.spring.domain.posts;
 
+import com.project.spring.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter //클래스 내 모든 필드의 Getter 메소드를 생성
 @NoArgsConstructor  //기본 생성자 자동 추가 - public Posts(){} 와 같은 효과
 @Entity //테이블과 링크될 클래스임을 나타낸다. 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭한다.
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id //해당 테이블의 PK
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성 규칙 스프링 부트 2.0에서는 GenerationType.IDENTITY 추가해야 auto_increment가 된다.
